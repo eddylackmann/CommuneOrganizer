64858ff0aa636b037dd1338fa117f9910071067f
eddylackmann <a.eddy@hotmail.de>
2017-07-06 10:50:12 +0200

    ...

--- app/src/main/java/de/commune/organizer/communeorganizer/login.java
+++ app/src/main/java/de/commune/organizer/communeorganizer/login.java
@@ -1,5 +1,6 @@
 package de.commune.organizer.communeorganizer;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -8,34 +9,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class login extends AppCompatActivity {
     private Button loginBtn;
+    private AlertDialog.Builder AlertBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        AlertBox = new AlertDialog.Builder(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
     }
 
 
     public void init(){
+         //= new AlertDialog.Builder(this)
         final TextView userEmail = (TextView)findViewById(R.id.uEmailText);
         final TextView userPassword = (TextView)findViewById(R.id.uPasswordText);
         loginBtn = (Button) findViewById(R.id.signInBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String uEmail =  userEmail.getText().toString();
-                String uPw = userPassword.getText().toString();
+                CharSequence uEmail =  userEmail.getText();
+                CharSequence uPw = userPassword.getText();
+                Intent intent = new Intent(login.this, Home.class);
+                startActivity(intent);
 
-                if(uEmail=="test@test.de" & uPw =="test"){
-                    Intent intent = new Intent(login.this, Home.class);
-                    startActivity(intent);
-                }else{
-
-                    userEmail.setTextColor(Color.RED);
-                    userPassword.setTextColor(Color.RED);
-                }
 
             }
         });
