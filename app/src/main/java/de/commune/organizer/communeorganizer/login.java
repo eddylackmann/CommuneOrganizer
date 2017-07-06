5adb48a0dc4a6704028476d02a46e7440486a50e
eddylackmann <a.eddy@hotmail.de>
2017-07-06 09:52:39 +0200

    ...

--- app/src/main/java/de/commune/organizer/communeorganizer/login.java
+++ app/src/main/java/de/commune/organizer/communeorganizer/login.java
@@ -1,10 +1,12 @@
 package de.commune.organizer.communeorganizer;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
     private Button loginBtn;
@@ -17,12 +19,24 @@ public class login extends AppCompatActivity {
 
 
     public void init(){
+        final TextView userEmail = (TextView)findViewById(R.id.uEmailText);
+        final TextView userPassword = (TextView)findViewById(R.id.uPasswordText);
         loginBtn = (Button) findViewById(R.id.signInBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(login.this, Home.class);
-                startActivity(intent);
+                String uEmail =  userEmail.getText().toString();
+                String uPw = userPassword.getText().toString();
+
+                if(uEmail=="test@test.de" & uPw =="test"){
+                    Intent intent = new Intent(login.this, Home.class);
+                    startActivity(intent);
+                }else{
+
+                    userEmail.setTextColor(Color.RED);
+                    userPassword.setTextColor(Color.RED);
+                }
+
             }
         });
     }
