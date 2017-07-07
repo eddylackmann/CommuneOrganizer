6c31df751acc8839744de4e5c107ea8c8557140c
eddylackmann <a.eddy@hotmail.de>
2017-07-07 07:46:53 +0200

    ...

--- app/src/main/java/de/commune/organizer/communeorganizer/login.java
+++ app/src/main/java/de/commune/organizer/communeorganizer/login.java
@@ -54,6 +54,7 @@ public class login extends AppCompatActivity implements AsyncResponse {
                 Intent intent = new Intent(login.this, Home.class);
                 startActivity(intent);
             }
+
         });
     }
 
