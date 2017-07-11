e3a009513ac8a5af68ec96463432e529a3d5ec92
eddylackmann <a.eddy@hotmail.de>
2017-07-11 11:59:33 +0200

    hzfdtsraetdzfjgjh

--- app/src/main/java/de/commune/organizer/communeorganizer/Home.java
+++ app/src/main/java/de/commune/organizer/communeorganizer/Home.java
@@ -81,9 +81,9 @@ public class Home extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_edit_users) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_commune_setting) {
 
         }
 
