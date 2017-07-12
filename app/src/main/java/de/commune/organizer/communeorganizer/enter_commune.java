8f623a6b3356eae8d7be32bd185eff2f8e691c6f
eddylackmann <a.eddy@hotmail.de>
2017-07-12 09:56:07 +0200

    new layout design

--- app/src/main/java/de/commune/organizer/communeorganizer/enter_commune.java
+++ app/src/main/java/de/commune/organizer/communeorganizer/enter_commune.java
@@ -2,6 +2,7 @@ package de.commune.organizer.communeorganizer;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,7 +21,9 @@ public class enter_commune extends AppCompatActivity implements AsyncResponse {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_commune);
+
         init();
+        setLayout();
     }
 
     public void init(){
@@ -28,10 +31,10 @@ public class enter_commune extends AppCompatActivity implements AsyncResponse {
         task = new PostResponseAsyncTask(this);
         controller = this;
 
-        final TextView communeID = (TextView)findViewById(R.id.communeID);
-        final TextView communePwText = (TextView)findViewById(R.id.communePwText);
+        final TextView communeID = (TextView)findViewById(R.id.enter_communeID);
+        final TextView communePwText = (TextView)findViewById(R.id.enter_communePwText);
         final String globalUserEmail = ((MyApplication) this.getApplication()).getUserEmail();
-        Button joinBtn = (Button) findViewById(R.id.joinBtn);
+        Button joinBtn = (Button) findViewById(R.id.enter_joinBtn);
         joinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +42,18 @@ public class enter_commune extends AppCompatActivity implements AsyncResponse {
                 task.execute("http://eddy-home.ddns.net/wg-app/loginMgt.php?Method=enterCommune&communeID=" + communeID.getText() + "&communePassword=" + communePwText.getText() + "&Email=" + globalUserEmail);
             }
         });
+    }
+
+
+    private void setLayout(){
+
+            TextView enter_communeID = (TextView) findViewById(R.id.enter_communeID);
+            enter_communeID.setHintTextColor(Color.WHITE);
+            TextView enter_communePW = (TextView) findViewById(R.id.enter_communePwText);
+            enter_communePW.setHintTextColor(Color.WHITE);
+
+
+
     }
 
     @Override
