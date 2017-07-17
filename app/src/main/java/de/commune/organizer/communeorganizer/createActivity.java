5e41f1fdcaecb5e53a10ce91855caa6666116845
Style4007 <tomtom9596@gmx.de>
2017-07-17 18:50:13 +0200

    Aktivität anlegen: Datumsfeld jetzt mit DateTimePicker

--- app/src/main/java/de/commune/organizer/communeorganizer/createActivity.java
+++ app/src/main/java/de/commune/organizer/communeorganizer/createActivity.java
@@ -1,10 +1,13 @@
 package de.commune.organizer.communeorganizer;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.kosalgeek.asynctask.AsyncResponse;
@@ -14,7 +17,7 @@ import com.kosalgeek.asynctask.PostResponseAsyncTask;
  * Created by tka on 13.07.2017.
  */
 
-public class createActivity extends AppCompatActivity implements AsyncResponse {
+public class createActivity extends AppCompatActivity implements AsyncResponse, DatePickerDialog.OnDateSetListener {
     public PostResponseAsyncTask task;
     public my_Library Lib;
     public AppCompatActivity controller;
@@ -45,7 +48,19 @@ public class createActivity extends AppCompatActivity implements AsyncResponse {
         communeID = ((MyApplication) this.getApplication()).getInformation("CommuneID");
 
         final EditText descActivity = (EditText) findViewById(R.id.descActivity);
+
+        Time today = new Time(Time.getCurrentTimezone());
+        today.setToNow();
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(this, android.app.AlertDialog.THEME_DEVICE_DEFAULT_LIGHT, createActivity.this,today.year,today.month,today.monthDay);
+
         final EditText dateActivity = (EditText) findViewById(R.id.dateActivity);
+        dateActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
         final EditText timeActivity = (EditText) findViewById(R.id.timeActivity);
 
         Button addActivity = (Button) findViewById(R.id.addActivity);
@@ -72,5 +87,35 @@ public class createActivity extends AppCompatActivity implements AsyncResponse {
             startActivity(intent);
         }
         task = new PostResponseAsyncTask(this);
+    }
+
+    private String formatDate(int day, int month, int year){
+        String sMonth;
+        String sDay;
+
+        if (day < 10)
+        {
+            sDay = "0" + day;
+        }
+        else
+        {
+            sDay = "" + day;
+        }
+
+        if (month < 10)
+        {
+            sMonth = "0" + month;
+        }
+        else
+        {
+            sMonth = "" + month;
+        }
+        return sDay + "." + sMonth + "." + year;
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        final EditText dateActivity = (EditText) findViewById(R.id.dateActivity);
+        dateActivity.setText(formatDate(i2,i1,i));
     }
 }
